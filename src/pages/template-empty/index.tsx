955d66733d8828a6279b86435600619cabe27a8b
方雨_Yu <304683191@qq.com>
2019-10-23 00:46:43 +0800

    perf: 调整 空模板的初始化内容

--- src/pages/template-empty/index.tsx
+++ src/pages/template-empty/index.tsx
@@ -1,76 +1,50 @@
-import { ComponentType } from 'react'
-import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
+import { ComponentType } from 'react';
+import Taro, { Component, Config } from '@tarojs/taro';
+import { View, Button, Text } from '@tarojs/components';
+import { observer, inject } from '@tarojs/mobx';
 
-import './index.scss'
-
-
-interface IMenus {
-  /**
-   * 名称
-   */
-  labbel: string,
-  /**
-   * 路径
-   */
-  path: string,
-}
+import './index.scss';
 
 interface IProps {} 
 
-interface IState {
-  /**
-   * 导航菜单
-   */
-  menus: Array<IMenus>,
-}
+interface IState {}
 
-interface Tutorials {
+interface Index {
   props: IProps;
   state: IState;
 }
 
-class Tutorials extends Component {
-
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
+@inject()
+@observer
+class Index extends Component {
   config: Config = {
     navigationBarTitleText: '入门'
   }
 
-  state: IState = {
-    menus: [
-      { labbel: 'State & Props', path: '/started/state-props' },
-    ],
-  }
+  state: IState = {}
 
-  componentWillMount () { }
+  componentWillMount() { }
 
-  componentWillReact () {}
+  componentWillReact() {}
 
-  componentDidMount () { }
+  componentDidMount() { }
 
-  componentWillUnmount () { }
+  componentWillUnmount() { }
 
-  componentDidShow () { }
+  componentDidShow() { }
 
-  componentDidHide () { }
+  componentDidHide() { }
 
   render () {
-    const { menus } = this.state
-    console.log(menus);
 
     return (
-      <View className='tutorials'>
-        <Text>counter</Text>
+      <View className='container'>
+        <Button>
+          <Text>counter</Text>
+        </Button>
       </View>
     )
   }
 }
 
-export default Tutorials  as ComponentType
+export default Index  as ComponentType
