7127d7827ece010179631af730bb2b8a40c0ac19
sufangyu <304683191@qq.com>
2020-08-11 00:12:42 +0800

    feat: 增加 结果展示组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,5 @@ export { default as ButtonCountDown } from './button-count-down';
 export { default as ImagePicker } from './image-picker';
 
 export { default as ImagePreview } from './image-preview';
+
+export { default as Result } from './result';
---
+++ src/components/result/index.tsx
@@ -0,0 +1,43 @@
+import Taro, { FC } from '@tarojs/taro';
+import { View, Image } from '@tarojs/components';
+import './index.scss';
+import icSuccess from './icons/success.png';
+import icFail from './icons/fail.png';
+import icWarning from './icons/warning.png';
+
+interface IProps {
+  /** 类型 */
+  type?: 'success' | 'fail' | 'warning';
+  /** 图标地址 */
+  imgUrl?: string;
+  /** title 文案 */
+  title: string;
+  /** message 文案 */
+  message?: string;
+}
+
+const Index: FC<IProps> = (props: IProps) => {
+  const { type, title, message } = props;
+  // eslint-disable-next-line react/destructuring-assignment
+  let imgUrl = props.imgUrl || '';
+  const IMAGES_MAP = {
+    success: icSuccess,
+    fail: icFail,
+    warning: icWarning,
+  };
+  
+  // eslint-disable-next-line react/destructuring-assignment
+  if (!props.imgUrl && type) {
+    imgUrl = IMAGES_MAP[type];
+  }
+
+  return (
+    <View className="comp-result">
+      <View className="result-image"><Image src={imgUrl} /></View>
+      <View className="result-title">{title}</View>
+      <View className="result-message">{message}</View>
+    </View>
+  );
+};
+
+export default Index;
